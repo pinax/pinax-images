01ae7072f0ebb635bcfbf197e2299e339991530f
Patrick Altman <paltman@gmail.com>
2016-04-10 11:06:58 -0500

    0.2.0

--- docs/changelog.md
+++ docs/changelog.md
@@ -1,5 +1,9 @@
 # Change Log
 
+## 0.2.0
+
+* Make DUA an optional requirement [PR #14](https://github.com/pinax/pinax-images/pull/14)
+
 ## 0.1.1
 
 * add Pillow to install requires
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description="an app for managing collections of images associated with a content object",
     name="pinax-images",
     long_description=read("README.md"),
-    version="0.1.1",
+    version="0.2.0",
     url="http://github.com/pinax/pinax-images/",
     license="MIT",
     packages=find_packages(),
