5d59de80d0c9e3a5f63cb8a11ae20cc0b17bbc03
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-03-15 00:43:57 +0100

    Drop unittest.mock() compatibility handling

--- pinax/images/compat.py
+++ pinax/images/compat.py
@@ -2,9 +2,3 @@ try:
     from account.mixins import LoginRequiredMixin
 except ImportError:
     from django.contrib.auth.mixins import LoginRequiredMixin  # noqa
-
-
-try:
-    from unittest import mock
-except ImportError:
-    import mock  # noqa
