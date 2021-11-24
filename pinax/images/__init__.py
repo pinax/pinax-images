8e82d18266f10ab93cc4e0b07bacba9b5ec109d2
Patrick Altman <paltman@gmail.com>
2021-11-23 21:07:35 -0600

    Still need for Dj22

--- pinax/images/__init__.py
+++ pinax/images/__init__.py
@@ -1,3 +1,8 @@
 import pkg_resources
 
+import django
+
+if django.__version__ > "3.2":  # Deprecated in 4.1 but needed in 2.2 still
+    default_app_config = "pinax.images.apps.AppConfig"
+
 __version__ = pkg_resources.get_distribution("pinax-images").version
